324c1464ea343f439f6e4ee606413cc101dadeb2
Jake Domb <jake.dombnz@gmail.com>
2023-11-04 16:27:19 +1300

    Muted status, and sink 1 -> @DEFAULT_SINK@

--- Shnob/volume.sh
+++ Shnob/volume.sh
@@ -1,8 +1,19 @@
 # This command should be made more robust
 
-volume=$(pactl get-sink-volume 1)
-
+volume=$(pactl get-sink-volume @DEFAULT_SINK@)
 volume=($volume)
 
-text=${volume[4]}
+muted=$(pactl get-sink-mute @DEFAULT_SINK@)
+muted=($muted)
+
+text="ha"
+
+if [ ${muted[1]} == no ]
+then
+    text=${volume[4]}
+else
+    text="MUTE"
+fi
+
+#text=${volume[4]}
 maintext="${text}" ./Shnob/block_prettify.sh
