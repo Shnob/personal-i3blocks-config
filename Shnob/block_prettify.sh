dbf824ede008e9041d6427dd68cb026d9da651fa
Jake Domb <jake.dombnz@gmail.com>
2023-11-05 13:16:57 +1300

    Changed block design

--- Shnob/block_prettify.sh
+++ Shnob/block_prettify.sh
@@ -4,4 +4,4 @@
 # $icontext
 # $maintext
 
-printf '<span foreground="#000000" background="'$maincolor'"> '$icontext' </span><span foreground="'$maincolor'" background="'$backcolor'"> %s </span>\n' "${maintext}"
+printf '<span foreground="'$maincolor'"></span><span foreground="#000000" background="'$maincolor'">'$icontext' </span><span foreground="'$maincolor'" background="'$backcolor'"> %s </span><span foreground="'$backcolor'"></span>\n' "${maintext}"
