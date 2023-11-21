08514622ad653e7ffa1ac49594b0fe97a7acd704
Jake Domb <jake.dombnz@gmail.com>
2023-11-21 23:45:19 +1300

    Added games to bar. Removed repeated variables in config

---
+++ Shnob/factorio.sh
@@ -0,0 +1,6 @@
+# This command should be made more robust
+
+./Shnob/game_launcher.sh
+
+# text=${volume[4]}
+# maintext="${text}" maincolor="#eea023e0" ./Shnob/block_prettify.sh
---
+++ Shnob/game_launcher.sh
@@ -0,0 +1,15 @@
+# This command should be made more robust
+
+# gamename
+# gameid
+# gameprocess
+
+text="$gamename"
+
+if [[ $button -eq 1 ]]; then
+    steam steam://rungameid/$gameid
+elif [[ $button -eq 3 ]]; then
+    pkill $gameprocess
+fi
+
+maintext="${text}" ./Shnob/block_prettify.sh
