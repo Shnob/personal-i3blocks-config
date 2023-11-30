381314659546e7b54b4a357d428eb7fb8e9a46cb
Jake Domb <jake.dombnz@gmail.com>
2023-11-30 18:24:58 +1300

    worldclock will now display the selected timezone, when clicked

--- Shnob/worldclock.sh
+++ Shnob/worldclock.sh
@@ -1,4 +1,12 @@
-if test -f Shnob/selected_timezone; then
-    text=$(TZ=$(pwd)/Shnob/selected_timezone date +%T)
-    maintext=$text ./Shnob/block_prettify.sh
+if [[ $button -eq 1 ]]; then
+    echo 5 > $(pwd)/Shnob/worldclock_display_counter
+fi
+if [[ $(cat $(pwd)/Shnob/worldclock_display_counter) -gt 0 ]]; then
+    echo $(( $(cat $(pwd)/Shnob/worldclock_display_counter) - 1 )) > $(pwd)/Shnob/worldclock_display_counter
+    maintext=$(readlink $(pwd)/Shnob/selected_timezone | sed -e "s/\/usr\/share\/zoneinfo\///") ./Shnob/block_prettify.sh
+else
+    if test -f Shnob/selected_timezone; then
+        text=$(TZ=$(pwd)/Shnob/selected_timezone date +%T)
+        maintext=$text ./Shnob/block_prettify.sh
+    fi
 fi
