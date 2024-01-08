84f4a2f82965f4a8ffb38dcbef4a43b3e10091d9
Shnob <jake.dombnz@gmail.com>
2024-01-08 22:21:53 +1300

    Clock can read out time

--- Shnob/clock.sh
+++ Shnob/clock.sh
@@ -1,2 +1,7 @@
+if [[ $button -eq 1 ]]; then
+    time_in_english=$(date "+%_M past %_I%p")
+    spd-say -t male3 "The time is $time_in_english"
+fi
+
 text=`date +%T`
 maintext=$text ./Shnob/block_prettify.sh
