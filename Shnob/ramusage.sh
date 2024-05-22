d97fd86e2ec4237e0be868c293854ca582529f80
Shnob <jake.dombnz@gmail.com>
2024-05-22 14:37:43 +1200

    Reworked ram usage

--- Shnob/ramusage.sh
+++ Shnob/ramusage.sh
@@ -5,8 +5,8 @@ mem=($mem)
 use=${mem[2]}
 tot=${mem[1]}
 
-use=$(echo $use | sed 's/\(.\)\(.\)..$/\1.\2/')
-tot=$(echo $tot | sed 's/\(.\)\(.\)..$/\1.\2/')
+use=$(echo "scale=2; $use / 1024" | bc)
+tot=$(echo "scale=2; $tot / 1024" | bc)
 
 text="${use}Gi / ${tot}Gi"
 
